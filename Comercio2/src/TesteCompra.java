e34dac48052157a202f6ac2a4fa5bc416e399bf4
gleisson <gleissonfreitas@hotmail.com>
2016-05-12 13:03:52 -0300

    //Commit1

---
+++ Comercio2/src/TesteCompra.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class TesteCompra {
+
+	@Test
+	public void compraAVista() {
+		Compra c = new Compra(500);
+		assertEquals(1, c.getNumeroParcelas());
+		assertEquals(500, c.getValorTotal());
+		assertEquals(500, c.getValorParcela());
+	}
+	
+	@Test
+	public void compraParcelada() {
+		Compra c = new Compra(4, 1000);
+		assertEquals(4, c.getNumeroParcelas());
+		assertEquals(4000, c.getValorTotal());
+		assertEquals(1000, c.getValorParcela());
+	}
+
+}
